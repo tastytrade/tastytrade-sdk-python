f2a1514e7b27df00479cdafc5db205ac9376dcbd
Aaron Mamparo <aaronmamparo@gmail.com>
2023-05-06 21:25:52 -0500

    er, set dev version tag instead

--- Makefile
+++ Makefile
@@ -4,8 +4,8 @@ clean:
 build: clean
 	pipenv run python -m build
 
-set_alpha_version_tag:
-	pipenv run python set_alpha_version_tag.py ${ALPHA_TAG}
+set_dev_version_tag:
+	pipenv run python set_dev_version_tag.py ${DEV_TAG}
 
 upload:
 	pipenv run python -m twine upload dist/* -u ${PYPI_USERNAME} -p ${PYPI_PASSWORD}
--- set_dev_version_tag.py
+++ set_dev_version_tag.py
@@ -3,12 +3,12 @@ from sys import argv
 from toml import load, dump
 
 if len(argv) < 2 or not argv[1]:
-    print('Alpha tag must be provided')
+    print('Dev tag must be provided')
     exit(1)
 
-alpha_tag = argv[1]
+dev_tag = argv[1]
 fname = 'pyproject.toml'
 pyproject = load(fname)
-pyproject['project']['version'] += f'a{alpha_tag}'
+pyproject['project']['version'] += f'dev{dev_tag}'
 with open(fname, 'w') as out:
     dump(pyproject, out)
