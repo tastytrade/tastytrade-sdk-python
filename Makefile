a151a7b66e1765c56ef382d7d0e9f650907355ed
Aaron Mamparo <aaronmamparo@gmail.com>
2023-05-06 20:49:12 -0500

    release tagged versions

--- Makefile
+++ Makefile
@@ -4,5 +4,8 @@ clean:
 build: clean
 	pipenv run python -m build
 
+tag_version:
+	pipenv run python tag_version.py "${TAG}"
+
 upload:
-	pipenv run python -m twine upload dist/* -u ${PYPI_USERNAME} -p ${PYPI_PASSWORD} --skip-existing
+	pipenv run python -m twine upload dist/* -u ${PYPI_USERNAME} -p ${PYPI_PASSWORD}
---
+++ tag_version.py
@@ -0,0 +1,14 @@
+from sys import argv
+
+from toml import load, dump
+
+if len(argv) < 2 or not argv[1]:
+    print('Tag must be provided')
+    exit(1)
+
+tag = argv[0]
+fname = 'pyproject.toml'
+pyproject = load(fname)
+pyproject['project']['version'] += f'-{tag}'
+with open(fname, 'w') as out:
+    dump(pyproject, out)
