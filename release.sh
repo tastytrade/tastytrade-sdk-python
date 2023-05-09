fa978cbed902dd31fc1cd89b6a47ac3816270a00
Aaron Mamparo <aaronmamparo@gmail.com>
2023-05-09 18:58:34 -0500

    fix release

--- release.sh
+++ release.sh
@@ -1,21 +1,33 @@
 #!/bin/zsh
 
+die() { echo "$*" 1>&2 ; exit 1; }
+
+read_release_type() {
+  export ERROR_MESSAGE="Usage: './release.sh (patch|minor|major)'"
+  if [ -z "$1" ]; then
+    die "${ERROR_MESSAGE}"
+  fi
+  if ! [[ $1 =~ (patch|minor|major) ]] ; then
+    die "${ERROR_MESSAGE}"
+  fi
+  echo "$1"
+}
+
+release_type="$(read_release_type $1)"
+
 if [[ "$(git rev-parse --abbrev-ref HEAD)" != "master" ]]; then
-  echo 'Release should be run on master. Exiting.';
-  exit 1;
+  die 'Release should be run on master. Exiting.'
 fi
 
 if [[ -n $(git status -s) ]]; then
-  echo "There are uncommitted changes. Exiting."
-  exit 1;
+  die "There are uncommitted changes. Exiting."
 fi
 
 if [[ $(git rev-parse HEAD) != $(git rev-parse master@{upstream}) ]]; then
-  echo 'Local master is not in sync with remote. Exiting.';
-  exit 1;
+  die 'Local master is not in sync with remote. Exiting.'
 fi
 
-export NEW_VERSION="$(poetry version patch --short)"
+export NEW_VERSION="$(poetry version ${release_type} --short)"
 git add pyproject.toml
 git commit -m "Release ${NEW_VERSION}"
 git tag ${NEW_VERSION}
