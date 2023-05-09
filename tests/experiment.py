441fa50d76b9b272835fdd858c1e03a73bf1c5a0
Aaron Mamparo <aaronmamparo@gmail.com>
2023-05-09 18:48:16 -0500

    release stuff

--- tests/experiment.py
+++ tests/experiment.py
@@ -12,6 +12,6 @@ load_dotenv()
 class Experiment(TestCase):
     def test_experiment(self):
         tastytrade = Tastytrade()
-        tastytrade.login(environ.get('TASTYTRADE_LOGIN'), environ.get('TASTYTRADE_PASSWORD'))
-        for equity in tastytrade.instruments.get_active_equities(lendability=Lendability.EASY_TO_BORROW):
-            print(equity)
+        # tastytrade.login(environ.get('TASTYTRADE_LOGIN'), environ.get('TASTYTRADE_PASSWORD'))
+        # for equity in tastytrade.instruments.get_active_equities(lendability=Lendability.EASY_TO_BORROW):
+        #     print(equity)
