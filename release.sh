7b4977c308f36ee7224307961ac11d1d91aad607
Aaron Mamparo <aaronmamparo@gmail.com>
2023-05-09 17:10:49 -0500

    release checks

--- release.sh
+++ release.sh
@@ -5,3 +5,7 @@ if [[ "$(git rev-parse --abbrev-ref HEAD)" != "master" ]]; then
   exit 1;
 fi
 
+if [[ $(git rev-parse HEAD) != $(git rev-parse master@{upstream}) ]]; then
+  echo 'Local master is not in sync with remote. Exiting.';
+  exit 1;
+fi
