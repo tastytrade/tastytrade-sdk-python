c4cea1fa0b883503fc5fd3237002d2788eec819e
Aaron Mamparo <aaronmamparo@gmail.com>
2023-11-17 12:01:30 -0600

    fix lint

--- tests/market_data_experiment.py
+++ tests/market_data_experiment.py
@@ -1,12 +1,10 @@
-import time
-
 from tests.utils import get_tasty
 
 
 def main():
     tasty = get_tasty()
     symbols = ['YELP  240517C00042000', 'AAPL']
-    subscription = tasty.market_data.subscribe(symbols=symbols, on_quote=print, on_candle=print, on_greeks=print, on_trade=print)
+    subscription = tasty.market_data.subscribe(symbols=symbols, on_quote=print, on_candle=print, on_greeks=print)
     subscription.open()
 
 
